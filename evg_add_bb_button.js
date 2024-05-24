8ee77f6efa13abacfa48cf238e9c5b59af7e48a7
Tommaso Tocci <tommaso.tocci@mongodb.com>
2024-05-24 08:56:56 +0200

    Bump version to 1.1

--- evg_add_bb_button.js
+++ evg_add_bb_button.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://spruce.mongodb.com/task/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 5/23/2024, 9:37:28 AM
 // ==/UserScript==
